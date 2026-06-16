2a9b88b808f1d9502b3a177994aed1f4e054b485
surendevops75 <surendevops75@gmail.com>
2026-06-16 10:02:43 +0530

    catalogue-unit-tests

---
+++ index.js
@@ -0,0 +1,27 @@
+# Complete Code
+
+```javascript
+const app = require('./server');
+
+const pino = require('pino');
+
+const logger = pino({
+
+  level: 'info',
+
+  prettyPrint: false,
+
+  useLevelLabels: true
+});
+
+const port =
+  process.env.CATALOGUE_SERVER_PORT || '8080';
+
+app.listen(port, () => {
+
+  logger.info('Started on port', port);
+
+});
+```
+
+---
